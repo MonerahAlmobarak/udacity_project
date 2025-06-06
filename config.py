79a59d748c4be2ba11175d40a47b163f6d545565
Monerah Almobarak <136607948+MonerahAlmobarak@users.noreply.github.com>
2025-06-06 04:01:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,34 +1,13 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+import os  
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '3b7d!@Ud4c1tyCMS#2025'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'image11project'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'your-storage-key'
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER')
-    SQL_DATABASE = os.environ.get('SQL_DATABASE')
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = (
-        'mssql+pyodbc://{username}:{password}@{server}:1433/{database}'
-        '?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no&Connection+Timeout=30'
-        .format(
-            username=os.environ.get('SQL_USER_NAME'),
-            password=os.environ.get('SQL_PASSWORD'),
-            server=os.environ.get('SQL_SERVER'),
-            database=os.environ.get('SQL_DATABASE')
-        )
-    )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-    AUTHORITY = "https://login.microsoftonline.com/common"
-    CLIENT_ID = os.environ.get('CLIENT_ID')
-    REDIRECT_PATH = "/getAToken"
-    SCOPE = ["User.Read"]
-    SESSION_TYPE = "filesystem"
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms1.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cmsadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'CMS4dmin'
